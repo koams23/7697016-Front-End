76786d1309a442cc62c1e52ccd7e3eb16df8c0a5
koams23 <129339410+koams23@users.noreply.github.com>
2023-05-05 20:28:22 +0200

    suite5.5

--- pieces.js
+++ pieces.js
@@ -2,7 +2,7 @@
 const reponse = await fetch ("pieces-autos.json");
 const pieces = await reponse.json();
 
-const article = pieces [0];
+{const article = pieces [0];
 const imageElement = document.createElement ("img");
 imageElement.src = article.image;
 const nomElement = document.createElement("h2");
@@ -11,11 +11,15 @@ const prixElement = document.createElement("p");
 prixElement.innerText = ` Prix : ${article.prix} €  (${article.prix <35 ? "€" : "€€€"})`;
 const categorieElement = document.createElement("p");
 categorieElement.innerText = article.categorie  ?? "(aucune catégorie)"; 
+const descriptionElement = document.createElement ("p");
+descriptionElement.innerText = article.description ?? "Pas encore de descriptif ";
+const disponibiliteElement = document.createElement ("p");
+disponibiliteElement.innerText = article.disponibilite ? "En stock": "En rupture de stock " ;
 
 const sectionFiches = document.querySelector(".fiches");
 sectionFiches.appendChild(imageElement);
 sectionFiches.appendChild(nomElement);
 sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
-
-
+sectionFiches.appendChild(descriptionElement);
+sectionFiches.appendChild(disponibiliteElement)}
