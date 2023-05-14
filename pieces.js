a16baf7b64ab3fc5036199a04899eaeaca7728c2
koams23 <129339410+koams23@users.noreply.github.com>
2023-05-15 01:42:16 +0200

    15.1

--- pieces.js
+++ pieces.js
@@ -57,7 +57,7 @@ const boutonFiltrer = document.querySelector(".btn-filtrer");
 
 boutonFiltrer.addEventListener("click",function(){
     const piecesFiltrees = pieces.filter(function(pieces){
-       return  pieces.prix<35 
+       return  pieces.prix<35; 
     });
     document.querySelector(".fiches").innerHTML = "";
     genererPieces(piecesFiltrees);
@@ -69,7 +69,7 @@ const boutonDecroissant = document.querySelector(".btn-decroissant");
 boutonDecroissant.addEventListener("click",function(){
     const piecesOrdonnees = Array.from(pieces);
     piecesOrdonnees.sort(function(a, b) {
-        return b.prix - a.prix
+        return b.prix - a.prix;
     });
     document.querySelector(".fiches").innerHTML = "";
     genererPieces(piecesOrdonnees);
@@ -86,7 +86,15 @@ boutonNodescription.addEventListener("click", function(){
     genererPieces(piecesFiltree);
 });
 
+const inputPrixMax = document.querySelector("#prix-max");
 
+inputPrixMax.addEventListener("input", function(){
+    const piecesFiltrees = pieces.filter(function(pieces){
+        return pieces.prix<= inputPrixMax.value;
+    });
+    document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesFiltrees)
+});
 
 
 
